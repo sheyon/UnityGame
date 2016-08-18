e947645834594afc394666980ce0acda96ba86d0
sheyon <jessicagriggs@gmail.com>
2016-08-18 15:57:54 -0400

    Add files via upload

---
+++ Scripts/LureBehaviors.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class LureBehaviors : MonoBehaviour {
+
+    public GameObject thing;
+    public Rigidbody rb;
+    public ThrowLure throwLure;
+
+    private bool lureActive;
+
+    [HideInInspector] public float despawnTime = 15f;
+
+    void Start()
+    {
+        lureActive = true;
+        rb = GetComponent<Rigidbody>();
+        throwLure = FindObjectOfType(typeof(ThrowLure)) as ThrowLure;
+    }
+
+    void Update()
+    {
+        if (lureActive == true)
+        {
+            rb.AddForce((throwLure.calculateBestThrowSpeed(throwLure.throwPoint.transform.position, throwLure.adjustedHit, 1f) * 100f), ForceMode.Force);
+            lureActive = false;
+        }
+
+        despawnTime -= Time.deltaTime;
+
+        if (despawnTime <= 0)
+        {
+            Destroy(thing);
+        }
+
+    }
+
+}
---
+++ Scripts/ThrowLure.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+
+public class ThrowLure : MonoBehaviour {
+
+    public GameObject throwPoint;
+    public GameObject crosshair;
+    public GameObject lure;
+    public LureBehaviors lureBehaviors;
+    [HideInInspector] public Vector3 adjustedHit;
+
+    public float maxDistance = 10f;
+    private RaycastHit hit;
+    private float distance;
+
+
+    void Start()
+    {
+
+    }
+
+
+    public Vector3 calculateBestThrowSpeed(Vector3 origin, Vector3 target, float timeToTarget)
+    {
+        // calculate vectors
+
+        Vector3 toTarget = target - origin;
+        Vector3 toTargetXZ = toTarget;
+        toTargetXZ.y = 0;
+
+        // calculate xz and y
+
+        float y = toTarget.y;
+        float xz = toTargetXZ.magnitude;
+
+        // calculate starting speeds for xz and y. Physics forumulase deltaX = v0 * t + 1/2 * a * t * t
+        // where a is "-gravity" but only on the y plane, and a is 0 in xz plane.
+        // so xz = v0xz * t => v0xz = xz / t
+        // and y = v0y * t - 1/2 * gravity * t * t => v0y * t = y + 1/2 * gravity * t * t => v0y = y / t + 1/2 * gravity * t
+
+        float t = timeToTarget;
+        float v0y = y / t + 0.5f * Physics.gravity.magnitude * t;
+        float v0xz = xz / t;
+
+        // create result vector for calculated starting speeds
+
+        Vector3 result = toTargetXZ.normalized;        // get direction of xz but with magnitude 1
+        result *= v0xz;                                // set magnitude of xz to v0xz (starting speed in xz plane)
+        result.y = v0y;                                // set y to v0y (starting speed of y plane)
+
+        return result;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            //ThrowLure();
+            Physics.Raycast(crosshair.transform.position, crosshair.transform.forward, out hit, maxDistance);
+
+            if (hit.collider == null)
+            {
+                print("You did not hit anything!");
+            }
+            else
+            {
+                distance = hit.distance;
+                print(distance);
+                Quaternion rotation = Quaternion.identity;
+
+                if (hit.distance <= maxDistance)
+                {
+                    adjustedHit = new Vector3(hit.point.x, (hit.point.y + 0.5f), hit.point.z);
+
+                    Instantiate(lure, throwPoint.transform.position, rotation);
+                }
+                else
+                {
+                    //do nothing
+                }
+            }
+        }
+    }
+}
