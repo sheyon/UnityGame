4b271528e0ec4838142171cd57447c5f0f3f66bf
sheyon <jessicagriggs@gmail.com>
2016-08-20 16:53:50 -0400

    Add files via upload

---
+++ Scripts/StateMachine/FollowLeaderState.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+
+public class FollowLeaderState : ITargetState
+{
+    private readonly StatePatternTarget target;
+    private Vector3 leaderNoFly;
+
+    public FollowLeaderState(StatePatternTarget statePatternTarget)
+    {
+        target = statePatternTarget;
+    }
+
+    public void UpdateState()
+    {
+        target.navMeshAgent.speed = target.followSpeed;
+        target.navMeshAgent.stoppingDistance = target.followStoppingDist;
+
+        //If I have a leader, follow them.
+        if (target.leader != null)
+        {
+            leaderNoFly = new Vector3(target.leader.transform.position.x, 0.5f, target.leader.transform.position.z);
+            Following();
+        }
+
+        //If my leader is destroyed, revert to Idle behaviors.
+        else if (target.leader == null)
+        {
+            Idle();
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && target.fearPlayer == true)
+        {
+            Repulsed();
+        }
+        if (other.gameObject.CompareTag("Feared"))
+        {
+            Repulsed();
+        }
+        if (other.gameObject.CompareTag("Lure") && target.attractable == true)
+        {
+            Attracted();
+        }
+        if (other.gameObject == target.leader)
+        {
+            Following();
+        }
+    }
+
+    public void Idle()
+    {
+        target.currentLocation = target.transform.position;
+        target.currentState = target.idleState;
+    }
+
+    public void FollowLeader()
+    {
+        Debug.Log("Can't transition to same state: FollowLeader");
+    }
+
+    public void Repulsed()
+    {
+        target.currentState = target.repulsedState;
+    }
+
+    public void Patrol()
+    {
+        Debug.Log("A target that is in a FollowLeader state cannot be patrolling");
+    }
+
+    public void Attracted()
+    {
+        target.currentState = target.attractedState;
+    }
+
+    // -----
+
+    //This method is needed so targets can begin following their leaders ONLY when certain conditions are met.
+    void Following()
+    {
+        if (Vector3.Distance(target.transform.position, target.leader.transform.position) >= target.beginFollowingDist)
+        {
+            target.navMeshAgent.ResetPath();
+            Idle();
+        }
+        else
+        {
+            target.navMeshAgent.SetDestination(leaderNoFly);
+        }
+    }
+}
---
+++ Scripts/StateMachine/StatePatternTarget.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+
+public class StatePatternTarget : MonoBehaviour
+{
+    //Multi-behavior parameters
+    [HideInInspector] public Vector3 currentLocation;
+    [HideInInspector] public GameObject player;
+    
+    //Repulsion parameters
+    public bool fearPlayer;
+    public bool freezeInPlace;
+    [HideInInspector] public GameObject fearedObject;
+    [HideInInspector] public float fearedSpeed = 5f;
+    [HideInInspector] public float fearedDistance = 5f;
+    [HideInInspector] public Vector3 previousLocation;
+
+    //Attracted parameters
+    public bool attractable;
+
+    //FollowLeader parameters
+    public GameObject leader;
+    public float followSpeed = 3.65f;
+    public float followStoppingDist = 3f;
+    public float beginFollowingDist = 10f;
+
+    //Idle parameters
+    public float idleSpeed = 1.5f;
+    public float idleStoppingDist = 0f;
+    public float idleRange = 2f;
+
+    //Patrol parameters
+    public bool startPatrolling;
+    public bool loopWaypoints;
+    public GameObject[] allWaypoints;
+    public GameObject despawnPoint;
+    public float patrolSpeed = 3.5f;
+
+    [HideInInspector] public ITargetState currentState;
+    [HideInInspector] public IdleState idleState;
+    [HideInInspector] public PatrolState patrolState;
+    [HideInInspector] public FollowLeaderState followLeaderState;
+    [HideInInspector] public RepulsedState repulsedState;
+    [HideInInspector] public AttractedState attractedState;
+    [HideInInspector] public NavMeshAgent navMeshAgent;
+
+    private void Awake()
+    {
+        idleState = new IdleState(this);
+        patrolState = new PatrolState(this);
+        followLeaderState = new FollowLeaderState(this);
+        repulsedState = new RepulsedState(this);
+        attractedState = new AttractedState(this);
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Start()
+    {
+        currentLocation = transform.position;
+        DoIFearPlayer();
+        StartingBehaviors();
+    }
+
+    public void DoIFearPlayer()
+    {
+        if (fearPlayer == true)
+        {
+            fearedObject = GameObject.FindGameObjectWithTag("Player");
+        }
+    }
+
+    void StartingBehaviors()
+    {
+        //Valid starting behaviors are FollowLeader, Patrolling, Idle
+        if (leader != null)
+        {
+            currentState = followLeaderState;
+        }
+        else if (startPatrolling == true)
+        {
+            currentState = patrolState;
+        }
+        else if (startPatrolling == false)
+        {
+            currentState = idleState;
+        }
+        else
+        {
+            Debug.Log("You have not set valid starting parameters; defaulting to Idle state");
+        }
+    }
+
+    void Update()
+    {
+        currentState.UpdateState();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        currentState.OnTriggerEnter(other);
+    }
+
+}
