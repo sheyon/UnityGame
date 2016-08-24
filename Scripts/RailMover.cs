f1f5da99e7463b3c49da4f87af328c5fab2b7a41
sheyon <jessicagriggs@gmail.com>
2016-08-24 14:46:21 -0400

    Add files via upload

--- Scripts/RailMover.cs
+++ Scripts/RailMover.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class RailMover : MonoBehaviour {
 
+    [HideInInspector] public FirstPersonController firstPersonController;
+    public ControlledBySwitch controlledBySwitch;
+
     public GameObject mover;
+    public float moverVertOffset = 1f;
     public float speed = 2.5f;
     public Transform[] waypoint;
     public bool loopWaypoints;
     public bool continueWithoutPlayer;
 
     public bool lockMovement;
-    public ControlledBySwitch controlledBySwitch;
+    private Vector3 moverOffsetPos;
 
     private GameObject player;
     private bool playerIsRiding;
@@ -21,6 +26,8 @@ public class RailMover : MonoBehaviour {
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        firstPersonController = FindObjectOfType<FirstPersonController>();
+
         if (controlledBySwitch == null)
         {
             readyToStart = true;
@@ -49,6 +56,13 @@ public class RailMover : MonoBehaviour {
     {
         if (rideIsFinished == false)
         {
+            if (lockMovement == true)
+            {
+                moverOffsetPos = new Vector3(mover.transform.position.x, (mover.transform.position.y + moverVertOffset), mover.transform.position.z);
+                firstPersonController.canFreeMove = false;
+                player.transform.position = Vector3.MoveTowards(player.transform.position, moverOffsetPos, Time.deltaTime * speed);
+            }
+
             if (loopWaypoints == true && i >= waypoint.Length) { i = 0; }
             mover.transform.position = Vector3.MoveTowards(mover.transform.position, waypoint[i].transform.position, Time.deltaTime * speed);
 
@@ -58,6 +72,7 @@ public class RailMover : MonoBehaviour {
                 if (loopWaypoints == false && i >= waypoint.Length)
                 {
                     rideIsFinished = true;
+                    firstPersonController.canFreeMove = true;
                 }
             }
         }
@@ -75,6 +90,7 @@ public class RailMover : MonoBehaviour {
         if (loopWaypoints == true)
         {
             rideIsFinished = false;
+            lockMovement = false;
         }
     }
 }
