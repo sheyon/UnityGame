f4b9729be0a571ea97a8d7d096b9b352c73cbe61
sheyon <jessicagriggs@gmail.com>
2016-08-25 15:46:05 -0400

    Add files via upload

---
+++ Scripts/CurveControlledBob.cs
@@ -0,0 +1,81 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+
+namespace UnityStandardAssets.Utility
+{
+    [Serializable]
+    public class CurveControlledBob
+    {
+        public float HorizontalBobRange = 0.33f;
+        public float VerticalBobRange = 0.33f;
+        public AnimationCurve Bobcurve = new AnimationCurve(new Keyframe(0f, 0f), new Keyframe(0.5f, 1f),
+                                                            new Keyframe(1f, 0f), new Keyframe(1.5f, -1f),
+                                                            new Keyframe(2f, 0f)); // sin curve for head bob
+        public float VerticaltoHorizontalRatio = 1f;
+
+        private float m_CyclePositionX;
+        private float m_CyclePositionY;
+        private float m_BobBaseInterval;
+        private float m_Time;
+
+        public FirstPersonController firstPersonController;
+        private float crouchedCameraPosX;
+        private float crouchedCameraPosY;
+
+
+        public void Setup(Camera camera, float bobBaseInterval)
+        {
+            m_BobBaseInterval = bobBaseInterval;
+            firstPersonController.m_OriginalCameraPosition = camera.transform.localPosition;
+
+            // get the length of the curve in time
+            m_Time = Bobcurve[Bobcurve.length - 1].time;
+        }
+
+
+        public Vector3 DoHeadBob(float speed)
+        {
+            if (firstPersonController.isCrouching == true)
+            {
+                crouchedCameraPosX = firstPersonController.m_OriginalCameraPosition.x * 1f;
+                crouchedCameraPosY = firstPersonController.m_OriginalCameraPosition.y * 0.5f;
+
+                float xPos = crouchedCameraPosX + (Bobcurve.Evaluate(m_CyclePositionX) * HorizontalBobRange);
+                float yPos = crouchedCameraPosY + (Bobcurve.Evaluate(m_CyclePositionY) * VerticalBobRange);
+
+                m_CyclePositionX += (speed * Time.deltaTime) / m_BobBaseInterval;
+                m_CyclePositionY += ((speed * Time.deltaTime) / m_BobBaseInterval) * VerticaltoHorizontalRatio;
+
+                if (m_CyclePositionX > m_Time)
+                {
+                    m_CyclePositionX = m_CyclePositionX - m_Time;
+                }
+                if (m_CyclePositionY > m_Time)
+                {
+                    m_CyclePositionY = m_CyclePositionY - m_Time;
+                }
+                return new Vector3(xPos, yPos, 0f);
+            }
+            else
+            {
+                float xPos = firstPersonController.m_OriginalCameraPosition.x + (Bobcurve.Evaluate(m_CyclePositionX) * HorizontalBobRange);
+                float yPos = firstPersonController.m_OriginalCameraPosition.y + (Bobcurve.Evaluate(m_CyclePositionY) * VerticalBobRange);
+
+                m_CyclePositionX += (speed * Time.deltaTime) / m_BobBaseInterval;
+                m_CyclePositionY += ((speed * Time.deltaTime) / m_BobBaseInterval) * VerticaltoHorizontalRatio;
+
+                if (m_CyclePositionX > m_Time)
+                {
+                    m_CyclePositionX = m_CyclePositionX - m_Time;
+                }
+                if (m_CyclePositionY > m_Time)
+                {
+                    m_CyclePositionY = m_CyclePositionY - m_Time;
+                }
+                return new Vector3(xPos, yPos, 0f);
+            }
+        }
+    }
+}
